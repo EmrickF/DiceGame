8aebc134d3817e70455cc0897b28d9273283470f
EmrickF <erik.finnberg07@gmail.com>
2025-10-02 13:44:07 +0300

    wdawdwa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,9 @@ import Dice from '../components/dice'
 import { useRouter } from "next/navigation"
 
 export default function Home() {
-  const [results, setResults] = useState<(number | null)[]>([null, null, null])
-  const [total, setTotal] = useState<number | null>(null)
+  const [results, setResults] = useState<number[]>([0, 0, 0])
+  const [total, setTotal] = useState<number | 0>(0)
   const [rolling, setRolling] = useState([false, false, false])
-  const [money, setMoney] = useState<number>(1000)
   const [message, setMessage] = useState<string>("")
   const [spinCount, setSpinCount] = useState<number>(0)
   const router = useRouter()
@@ -80,7 +79,7 @@ export default function Home() {
       if (newResults.every(val => val !== null)) {
         if (newResults[0] === newResults[1] && newResults[1] === newResults[2]) {
           const winValue = newResults[0]
-          const winAmount = winValue * 50
+          const winAmount = winValue * 100
           setMoney(m => m + winAmount)
           saveMoney()
           setTimeout(() => {
@@ -89,14 +88,18 @@ export default function Home() {
           }, 2000)
         } else {
           setMessage("Ingen vinst. Försök igen! 🤣🤣🤣")
+           saveMoney()
+
         }
       }
     }, 500)
   }
 
-  React.useEffect(() => {
+  useEffect(() => {
     setResults([null, null, null])
   }, [])
+
+ 
 
   return (
    <main className="flex flex-col select-none items-center justify-center min-h-screen
